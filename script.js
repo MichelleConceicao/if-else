6c3a50e25c1f9713daf487982a1483de40081851
MichelleConceição <liliandlentretenimento@gmail.com>
2022-06-13 18:09:53 -0300

    2° desafio finalizado

---
+++ script.js
@@ -0,0 +1,24 @@
+let idade = 18;
+ if(idade >= 18){
+  console.log("Você é maior de 18 anos");
+ }else{
+   console.log("Você nao tem idade.");
+}
+
+let maioridade = true;
+  console.log(maioridade);
+
+let aniversario = "Abril";
+ if((aniversario ==="Janeiro")||(aniversario ==="Dezembro")){
+    console.log("VocÊ faz aniversario no final ou começo do ano.");
+ }else{
+    console.log("Você nao e de Capricórnio.");
+}
+
+const nome = "Michelle";
+let letra = "M";
+if(letra === "R"){
+    console.log("Seu nome começa com R");
+}else{
+    console.log("Seu nome não começa com R")
+}
